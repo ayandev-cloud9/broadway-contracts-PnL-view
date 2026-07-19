c92b17983c25855a487ad5257690c256d3a1760c
ayandev-cloud9 <ayan.dev@broadwaylive.in>
2026-07-19 19:40:04 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Paste your published CSV link here (see README.txt for how to get it).
 // It must point at the "Contract Dump - Raw" tab specifically, output=csv.
-const CSV_URL = "PASTE_YOUR_PUBLISHED_CSV_LINK_HERE";
+const CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRP6czqcb_-XAT2tdwG7k6M97nZSQ0ICNCH2fQFyAegIdHQPiySfq0RJ5Xy84tD9RtXVi2TVXi8E9h7/pub?gid=0&single=true&output=csv";
 
 // How many days ahead counts as "expiring soon" in the watchlist.
 const WATCHLIST_DAYS = 60;
