581db9e9b061a6edcb97b6eb7006d12a0f6d78f5
ayandev-cloud9 <ayan.dev@broadwaylive.in>
2026-07-19 23:50:42 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,3 +5,5 @@ const CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRP6czqcb_-XAT2
 // How many days ahead counts as "expiring soon" in the watchlist.
 const WATCHLIST_DAYS = 60;
 const MAPPING_CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRP6czqcb_-XAT2tdwG7k6M97nZSQ0ICNCH2fQFyAegIdHQPiySfq0RJ5Xy84tD9RtXVi2TVXi8E9h7/pub?gid=762046841&single=true&output=csv"
+const PREV_MONTH_CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRP6czqcb_-XAT2tdwG7k6M97nZSQ0ICNCH2fQFyAegIdHQPiySfq0RJ5Xy84tD9RtXVi2TVXi8E9h7/pub?gid=522355177&single=true&output=csv"
+const FORECAST_CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRP6czqcb_-XAT2tdwG7k6M97nZSQ0ICNCH2fQFyAegIdHQPiySfq0RJ5Xy84tD9RtXVi2TVXi8E9h7/pub?gid=299407060&single=true&output=csv"
